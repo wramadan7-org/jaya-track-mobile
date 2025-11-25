7e37cb63c147db7ce3f95e5899cb48afb28a9006
Wahyu Ramadan <wramadan1203@gmail.com>
2025-11-25 07:31:14 +0700

    chore: add sentry to logs error/crash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,12 +11,32 @@ import ErrorBoundary from "@/components/error-boundary";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://6024dc8b3320a1c766572bfc7664c7da@o4510420810072064.ingest.us.sentry.io/4510420812955648',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
@@ -68,4 +88,4 @@ export default function RootLayout() {
       </KeyboardProvider>
     </SafeAreaProvider>
   );
-}
+});
